9e713655c5c40ebb72b980005de6e79b2b9de0bd
WangSF <soullose@qq.com>
2024-09-12 17:31:02 +0800

    添加路由

---
+++ flutter_bloc/bloc_mode/lib/router/app_router.dart
@@ -0,0 +1,30 @@
+import 'package:bloc_mode/app.dart';
+import 'package:bloc_mode/counter/view/counter_page.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  static final _rootNavigatorKey = GlobalKey<NavigatorState>();
+
+  static final GoRouter _router = GoRouter(
+    navigatorKey: _rootNavigatorKey,
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const AppPage(),
+        routes: <RouteBase>[
+          GoRoute(
+            path: 'blocCount',
+            builder: (context, state) => const CounterPage(),
+          ),
+        ],
+      ),
+    ],
+  );
+
+// GoRouter routes() {
+//   return GoRouter(routes: routes,refreshListenable: );
+// }
+
+  static GoRouter get router => _router;
+}
