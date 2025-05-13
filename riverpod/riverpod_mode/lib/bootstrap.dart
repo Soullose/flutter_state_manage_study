07f9369b34a4022fd22099f16fd4c534a440bea6
WangSF <soullose@qq.com>
2025-05-13 21:43:15 +0800

    refactor(项目结构): 新增 bootstrap.dart 文件并重构 main.dart- 新增 bootstrap.dart 文件，用于初始化和配置应用 - 重构 main.dart，通过调用 bootstrap函数启动应用 - 优化了应用的初始化逻辑，提高了代码的可读性和可维护性

---
+++ riverpod/riverpod_mode/lib/bootstrap.dart
@@ -0,0 +1,74 @@
+
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:riverpod_mode/route/app_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'common/state_logger.dart';
+import 'common/storage/shared_preferences_provider.dart';
+import 'generated/l10n.dart';
+
+Future<void> bootstrap() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  final SharedPreferencesAsync asyncPrefs = SharedPreferencesAsync();
+  runApp(
+    ProviderScope(
+      observers: [
+        // AppProviderObserver(),
+        StateLogger(),
+      ],
+      overrides: [
+        sharedPreferencesUtilsProvider,
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // builder: (context, child) {
+      //   return MaterialApp.router(
+      //     routerConfig: AppRouter.router,
+      //     theme: FlexThemeData.light(scheme: FlexScheme.bahamaBlue),
+      //     darkTheme: FlexThemeData.dark(scheme: FlexScheme.bahamaBlue),
+      //     localizationsDelegates: const [
+      //       S.delegate,
+      //       GlobalMaterialLocalizations.delegate,
+      //       GlobalCupertinoLocalizations.delegate,
+      //       GlobalWidgetsLocalizations.delegate
+      //     ],
+      //     supportedLocales: S.delegate.supportedLocales,
+      //   );
+      // },
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
+        theme: FlexThemeData.light(scheme: FlexScheme.bahamaBlue),
+        darkTheme: FlexThemeData.dark(scheme: FlexScheme.bahamaBlue),
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+      ),
+    );
+  }
+}
--- riverpod/riverpod_mode/lib/main.dart
+++ riverpod/riverpod_mode/lib/main.dart
@@ -1,72 +1,30 @@
-import 'package:flex_color_scheme/flex_color_scheme.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:riverpod_mode/common/storage/shared_preferences_provider.dart';
-import 'package:riverpod_mode/common/storage/shared_preferences_service.dart';
-import 'package:riverpod_mode/route/app_router.dart';
-import 'package:riverpod_mode/utils/app_provider_observer.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:async';
+import 'dart:developer';
 
-import 'common/state_logger.dart';
-import 'generated/l10n.dart';
+import 'bootstrap.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  final SharedPreferencesAsync asyncPrefs = SharedPreferencesAsync();
-  runApp(
-    ProviderScope(
-      observers: [
-        // AppProviderObserver(),
-        StateLogger(),
-      ],
-      overrides: [
-        sharedPreferencesUtilsProvider,
-      ],
-      child: const MyApp(),
-    ),
+  runZonedGuarded(
+    () async {
+      await bootstrap();
+    },
+    (error, stack) {
+      return log(error.toString(), stackTrace: stack);
+    },
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      // builder: (context, child) {
-      //   return MaterialApp.router(
-      //     routerConfig: AppRouter.router,
-      //     theme: FlexThemeData.light(scheme: FlexScheme.bahamaBlue),
-      //     darkTheme: FlexThemeData.dark(scheme: FlexScheme.bahamaBlue),
-      //     localizationsDelegates: const [
-      //       S.delegate,
-      //       GlobalMaterialLocalizations.delegate,
-      //       GlobalCupertinoLocalizations.delegate,
-      //       GlobalWidgetsLocalizations.delegate
-      //     ],
-      //     supportedLocales: S.delegate.supportedLocales,
-      //   );
-      // },
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        routerConfig: AppRouter.router,
-        theme: FlexThemeData.light(scheme: FlexScheme.bahamaBlue),
-        darkTheme: FlexThemeData.dark(scheme: FlexScheme.bahamaBlue),
-        localizationsDelegates: const [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate
-        ],
-        supportedLocales: S.delegate.supportedLocales,
-      ),
-    );
-  }
+  // WidgetsFlutterBinding.ensureInitialized();
+  // final SharedPreferences prefs = await SharedPreferences.getInstance();
+  // final SharedPreferencesAsync asyncPrefs = SharedPreferencesAsync();
+  // runApp(
+  //   ProviderScope(
+  //     observers: [
+  //       // AppProviderObserver(),
+  //       StateLogger(),
+  //     ],
+  //     overrides: [
+  //       sharedPreferencesUtilsProvider,
+  //     ],
+  //     child: const MyApp(),
+  //   ),
+  // );
 }
