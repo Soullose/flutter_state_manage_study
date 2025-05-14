a554b4633f8a31848791df6095429a2c6770174e
WangSF <soullose@qq.com>
2025-05-14 09:46:30 +0800

    feat(plugin): 添加 sqlite3_flutter_libs 插件支持
    - 在 Linux、Windows 和 macOS 平台的生成插件注册文件中添加 sqlite3_flutter_libs 插件
    - 更新各平台的生成插件列表，包含 sqlite3_flutter_libs
    - 对于 macOS，还添加了 path_provider_foundation 插件支持

--- riverpod/riverpod_mode/macos/Flutter/GeneratedPluginRegistrant.swift
+++ riverpod/riverpod_mode/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,9 @@ import dynamic_color
 import flutter_image_compress_macos
 import macos_ui
 import macos_window_utils
+import path_provider_foundation
 import shared_preferences_foundation
+import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppkitUiElementColorsPlugin.register(with: registry.registrar(forPlugin: "AppkitUiElementColorsPlugin"))
@@ -20,5 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
