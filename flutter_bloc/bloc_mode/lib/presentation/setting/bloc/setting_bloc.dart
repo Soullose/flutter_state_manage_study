a79364ee7c369b3525482bf987d5bc90af552988
WangSF <soullose@qq.com>
2025-05-20 21:46:06 +0800

    feat(setting): 添加设置变更事件处理
    - 在 SettingBloc 中添加 SettingChangeEvent 的处理逻辑
    - 新增 SettingChangeEvent 类，用于表示设置变更事件
    - 优化 SettingBloc构造函数，使用常量初始化初始状态

--- flutter_bloc/bloc_mode/lib/presentation/setting/bloc/setting_bloc.dart
+++ flutter_bloc/bloc_mode/lib/presentation/setting/bloc/setting_bloc.dart
@@ -3,12 +3,19 @@ import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'setting_event.dart';
+
 part 'setting_state.dart';
 
 class SettingBloc extends Bloc<SettingEvent, SettingState> {
-  SettingBloc() : super(SettingInitial('127.0.0.1', '8080')) {
+  SettingBloc() : super(const SettingInitial('127.0.0.1', '8080')) {
     on<SettingEvent>((event, emit) {
       // TODO: implement event handler
     });
+    on<SettingChangeEvent>(_onSettingChangeEvent);
+  }
+
+  void _onSettingChangeEvent(
+      SettingChangeEvent event, Emitter<SettingState> emit) {
+    emit(SettingInProgress(event.ipAddress, event.port));
   }
 }
--- flutter_bloc/bloc_mode/lib/presentation/setting/bloc/setting_event.dart
+++ flutter_bloc/bloc_mode/lib/presentation/setting/bloc/setting_event.dart
@@ -1,7 +1,7 @@
 part of 'setting_bloc.dart';
 
 @immutable
-sealed class SettingEvent extends  Equatable{
+sealed class SettingEvent extends Equatable {
   const SettingEvent();
 
   @override
@@ -10,5 +10,20 @@ sealed class SettingEvent extends  Equatable{
   @override
   String toString() {
     return 'SettingEvent{}';
+  }
+}
+
+final class SettingChangeEvent extends SettingEvent {
+  final String ipAddress;
+  final String port;
+
+  const SettingChangeEvent(this.ipAddress, this.port);
+
+  @override
+  List<Object> get props => [ipAddress, port];
+
+  @override
+  String toString() {
+    return 'SettingChangeEvent{ipAddress: $ipAddress, port: $port}';
   }
 }
