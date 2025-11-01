82eb054f901db02e377fe75318b7ba04c4cbf2a1
WangSF <soullose@qq.com>
2025-11-01 20:45:47 +0800

    refactor(mqtt): 移除MQTT状态管理的Cubit实现
    - 删除MqttCubit类及其相关状态管理逻辑- 移除对bloc和equatable包的依赖导入- 删除MqttState密封类及MqttInitial初始状态类- 清理MQTT模块中不再使用的状态管理代码

--- flutter_bloc/bloc_mode/lib/common/mqtt/bloc/mqtt_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part 'mqtt_state.dart';
-
-class MqttCubit extends Cubit<MqttState> {
-  MqttCubit() : super(MqttInitial());
-}
