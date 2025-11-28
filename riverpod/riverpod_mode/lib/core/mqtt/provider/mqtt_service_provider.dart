c0e140729bdbebf621b26701d2761419d8a39297
soullose <soullose@qq.com>
2025-11-28 22:49:17 +0800

    feat(mqtt): 使用Riverpod提供MQTT服务和消息流
    - 创建`mqtt_service_provider.dart`以集成MQTT服务与Riverpod。
    - 定义一个`@Riverpod(keepAlive: true)`提供者`mqttServerClientService`，用于全局实例化并持有`MqttServerClientService`。
    - 新增一个`@riverpod`流提供者`mqttMessage`，它监听`mqttServerClientService`并暴露其`messageStream`，以便UI或其他部分可以响应式地接收MQTT消息。

---
+++ riverpod/riverpod_mode/lib/core/mqtt/provider/mqtt_service_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_mode/core/mqtt/mqtt_server_client_service.dart';
+
+part 'mqtt_service_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+MqttServerClientService mqttServerClientService(Ref ref) {
+  return MqttServerClientService();
+}
+
+/// 2. 核心：StreamProvider
+///
+/// 生成器会根据返回类型 Stream<Map<String, String>> 自动生成对应的 AutoDisposeStreamProvider
+@riverpod
+Stream<Map<String, String>> mqttMessage(Ref ref) {
+  /// 监听上面的 mqttServiceProvider
+  final service = ref.watch(mqttServerClientServiceProvider);
+  return service.messageStream;
+}
